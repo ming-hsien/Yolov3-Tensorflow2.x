f1bc5e29d5c1ed632f0c6b734d84d6d8d6be0f5c
ming-hsien <71801044+ming-hsien@users.noreply.github.com>
2021-11-23 09:45:44 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,13 +1,18 @@
 import os
 import cv2
+import glob
 import numpy as np
 import tensorflow as tf
-from yolov3.utils import detect_image, Load_Yolo_model
-from yolov3.configs import *
+from yolov4.utils import detect_image, Load_Yolo_model
+from yolov4.configs import *
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-image_path   = "./Data/test_image/test.jpg"
+img_dir = "./Data/test_image"
+save_dir = "./Data/results/"
+img_list = glob.glob(img_dir + '/*')
 
 yolo = Load_Yolo_model()
-detect_image(yolo, image_path, "./Data/test_image/results.jpg", input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+
+for img_path in img_list:
+    detect_image(yolo, image_path, os.path.join(save_dir,os.path.basename(img_path)), input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
